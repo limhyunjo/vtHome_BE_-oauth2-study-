4708fc70b5251e3db03d03eaf7681a55400cbb9b
limhyunjo <sibagyeoni@gmail.com>
2025-12-10 10:03:31 +0900

    소셜 로그인 기본 설정

---
+++ src/main/java/userservice/example/vthome_be/VtHomeBeApplication.java
@@ -0,0 +1,13 @@
+package userservice.example.vthome_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtHomeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VtHomeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/userservice/example/vthome_be/VtHomeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package userservice.example.vthome_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtHomeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
